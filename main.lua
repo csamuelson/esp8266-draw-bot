f4e1712f43a0b1b8867cca0d30d5cecb271837f0
chris_samuelson <chris@varsitynewsnetwork.com>
2015-11-08 22:15:32 -0500

    add left and right, fix some pen up/down and start/stop login, add networking

--- main.lua
+++ main.lua
@@ -1,17 +1,23 @@
+print("main.lua")
 left_stepper_pins = {8,6,5,7}
 right_stepper_pins = {4,2,1,3}
 pen_pin = 12
-pen_up_value = 75
-pen_down_value = 35
-pen_pwm_freq = 50 -- probably don't change this, it might not be good for the servos
-ms_between_steps = 3
-servo_time = 50 -- time in ms for servo to move / ms_between_steps
+pen_up_value = 35
+pen_down_value = 18
+wheel_dia = 68.5
+wheel_base = 112.5
+steps_rev = 512
+pen_pwm_freq = 25 -- probably don't change this, it might not be good for the servos
+ms_between_steps = 6
+servo_time = 30 -- time in ms for servo to move / ms_between_steps
 backward_order = {{1,0,1,0},{0,1,1,0},{0,1,0,1},{1,0,0,1}}
 forward_order = {{1,0,0,1},{0,1,0,1},{0,1,1,0},{1,0,1,0}}
 direction = "forward"
 active = 0
 step = 1
 final_run = 0
+drawing = 0
+remember_pen = 0
 
 -- setup pwm
 
@@ -22,6 +28,7 @@ active = servo_time
 
 function release()
    for pin=1,4 do
+      tmr.wdclr()
       gpio.mode(left_stepper_pins[pin],gpio.OUTPUT)
       gpio.write(left_stepper_pins[pin],0)
       gpio.mode(right_stepper_pins[pin],gpio.OUTPUT)
@@ -29,6 +36,9 @@ function release()
    end
 end
 
+function get_steps(distance)
+   return distance * steps_rev / (wheel_dia * 3.1412)
+end
 
 function move(dir)
    tmr.wdclr()
@@ -49,87 +59,144 @@ function move(dir)
       end
       active = active - 1
    elseif dir == "left" then
-   elseif dir == "right" then
-   elseif dir == "pen" then
-      print("active" .. active)
-      print("final_run" .. final_run)
-      if active == 1 then
-	 print("pwm stop")
-	 pwm.stop(pen_pin)
+      step = step + 1
+      if step > 4 then step = 1 end
+      for pin=1,4 do
+	 gpio.write(right_stepper_pins[pin],backward_order[step][pin])
+	 gpio.write(left_stepper_pins[pin],backward_order[step][pin])
       end
       active = active - 1
+   elseif dir == "right" then
+      step = step + 1
+      if step > 4 then step = 1 end
+      for pin=1,4 do
+	 gpio.write(right_stepper_pins[pin],forward_order[step][pin])
+	 gpio.write(left_stepper_pins[pin],forward_order[step][pin])
+      end
+      active = active - 1
+   elseif dir == "pen" then
+      active = active - 1
+--      print("active = " .. active)
+      if active == 1 then
+	 pwm.stop(pen_pin)
+      end
    end
 end
 
 function pen_up()
+   print('pen up func')
    pwm.setduty(pen_pin,pen_up_value)
 end
 
 function pen_down()
+   print('pen down func')
    pwm.setduty(pen_pin,pen_down_value)
 end
 
+function start_drawing()
+   if drawing == 0 then
+      print('start drawing')
+      file.open("commands.logo","r")
+      final_run = 0
+      drawing = 1
+   end
+end
+
+function stop_drawing()
+   drawing = 0
+   file.close()
+   release()
+   pen_up()
+   direction = "pen"
+   active = servo_time
+end
+
+-- make sure the motors are off
+
 release()
 
-tmr.alarm(0, 1000, 1, function()
-   if wifi.sta.getip() == nil then
-      print("Connecting to AP...")
-   else
-      print('IP: ',wifi.sta.getip())
-      tmr.stop(0)
-   end
+-- setup web service
+
+srv=net.createServer(net.TCP)
+srv:listen(80,function(conn)
+	      conn:on("receive",function(conn,payload)
+			 print(payload)
+			 postparse={string.find(payload,"start=")}
+			 httpparse={string.find(payload," HTTP")}
+			 start = "no"
+			 if postparse[2] ~= nil then
+			    start = string.sub(payload,postparse[2]+1,httpparse[1])
+			    print("start = " .. start .. ".")
+			 end
+			 conn:send("<h1>Draw Bot</h1><h2><a href='index.html?start=yes'>Start</a></h2><h2><a href='index.html?start=no'>Stop</a></h2>")
+			 if string.find(start,"yes") then
+			    direction = "pen"
+			    active = 500
+			    start_drawing()
+			 elseif string.find(start,"no") then
+			    stop_drawing()
+			 end
+			 conn.close(conn)
+	      end)
 end)
 
+-- open the drawing file
 
 
--- tmr.alarm(1, 6, 1, function()
--- tmr.wdclr()
--- 	     if step == 4 then step=1 else step=step+1 end
--- 	     print(step)
--- 	     for pin=1, 4 do
--- 		mypin = stepper_pins[pin]
--- 		if mypin == nil then
--- 		   print("mypin " .. pin .. "is nil")
--- 		else
--- 		   gpio.write(stepper_pins[pin],forward_order[step][pin])
--- 		end
--- 	     end
--- end)
 
-file.open("commands.logo","r")
+-- timer loop to execute drawing actions
 
 tmr.alarm(1, ms_between_steps, 1, function()
-	     if active > 0 then
+	     if drawing == 1 then
+		if active > 0 then
 		   move(direction)
-	     else
-		if final_run == 1 then
-		   tmr.stop(1)		   
 		else
-		   line = file.readline()
-		   if line == nil then
-		      release()
-		      pen_up()
-		      direction = "pen"
-		      active = servo_time
-		      final_run = 1
+		   if final_run == 1 then
+--		      tmr.stop(1)
+		      --		   node.dsleep(0,4)
 		   else
-		      print(line)
-		      direction = string.match(line, "%a+")
-		      if direction == "pen" then
-			 if string.sub(line,5,6) == "up" then
-			    pen_up()
-			    active = servo_time
-			 elseif string.sub(line,5,8) == "down" then
-			    pen_down()
-			    active = servo_time
-			 end
+		      line = file.readline()
+		      if line == nil then
+			 release()
+			 pen_up()
+			 direction = "pen"
+			 active = servo_time
+			 final_run = 1
 		      else
-			 active = tonumber(string.match(line, "%d+"))
-			 if active == nil then
-			    active = 0
+			 print("line: " .. line)
+			 direction = string.match(line, "%a+")
+			 if direction == "pen" then
+			    if string.sub(line,5,6) == "up" then
+			       pen_up()
+			       active = servo_time
+			    elseif string.sub(line,5,8) == "down" then
+			       print("going going down")
+			       pen_down()
+			       active = servo_time
+			    end
+			 else
+			    active = tonumber(string.match(line, "%d+"))
+			    if active == nil then
+			       active = 0
+			    elseif direction == "right" then
+			       rotation = active / 360.0
+			       distance = wheel_base * 3.1412 * rotation
+			       steps = get_steps(distance)
+			       active = steps * 4
+			    elseif direction == "left" then
+			       rotation = active / 360.0
+			       distance = wheel_base * 3.1412 * rotation
+			       steps = get_steps(distance)
+			       active = steps * 4
+			    end
 			 end
 		      end
 		   end
+		end
+	     else
+		if direction == "pen" and active > 0 then
+		   print('pen upping')
+		   move(direction)
 		end
 	     end
 end)
